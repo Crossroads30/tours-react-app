d1b9d5440ef6000980c0196b6a27b0e9462f3a1f
crossroads30 <crossroads30@mail.ru>
2023-12-29 17:01:36 +0300

    feat: add project structure

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,53 @@
 import React, { useState, useEffect } from 'react'
 import Loading from './Loading'
 import Tours from './Tours'
-// ATTENTION!!!!!!!!!!
-// I SWITCHED TO PERMANENT DOMAIN
+
 const url = 'https://course-api.com/react-tours-project'
+
 function App() {
-	return <h2>Tours Project Setup</h2>
+	const [loading, setLoading] = useState(true)
+	const [tours, setTours] = useState([])
+	const [isError, setIsError] = useState(false)
+
+	const fetchData = async () => {
+		setLoading(true)
+		try {
+			const response = await fetch(url)
+			if (!response.ok) {
+				// case for 'fetch' to handle '400th' & '500th' errors!
+				setIsError(true)
+				setIsFetching(false)
+				return
+			}
+			const tours = await response.json()
+			setLoading(false)
+			setTours(tours)
+			console.log(tours)
+		} catch (error) {
+			setLoading(false)
+			console.log(error)
+		}
+	}
+
+	useEffect(() => {
+		fetchData()
+	}, [])
+
+	if (loading) {
+		return (
+			<main>
+				<Loading />
+			</main>
+		)
+	}
+	if (isError) {
+		return <h3>There was an error!</h3>
+	}
+	return (
+		<main className='main'>
+			<Tours tours={tours} />
+		</main>
+	)
 }
 
 export default App
